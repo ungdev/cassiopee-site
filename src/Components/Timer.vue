f127916a46298ecd6f707b8217ec9374eefe22a0
Carrybou <thomasguibert94@gmail.com>
2024-04-07 16:33:50 +0200

    font accueil

--- src/Components/Timer.vue
+++ src/Components/Timer.vue
@@ -5,9 +5,14 @@
             Your browser does not support the video tag.
         </video>
         <div class="header">
-            <img src="../assets/Logo_or.png" width="30%">
-            <vue3-flip-countdown 
-            countdownSize="1.5rem" labelSize=".8rem" labelColor="#FFFFFF" 
+            <!--<img src="../assets/Logo_or.png" width="30%">-->
+            <div class="title">
+            <h1>Cassiopée</h1>
+            <h3>Festival UTT</h3>
+            <h2>Le Petit Prince</h2>
+            </div>
+            <vue3-flip-countdown class="countdown"
+            countdownSize="3rem" labelSize="1.2rem" labelColor="#FFFFFF" 
             deadlineISO="2024-06-01T16:17:13.481Z" 
             :flipAnimation="false" mainColor="#FFFFFF" secondFlipColor="#ffffff"/>
         </div>      
@@ -30,12 +35,34 @@ video {
   color: white;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items : center;
   height: 60%;
   text-shadow: 1px 1px 8px rgba(0,0,0,0.6);
 
 }
+.title{
+    display: flex;
+    flex-direction: column;
+    font-size: 3rem;
+    font-weight: bold;
+    gap: 20px;
+    text-align: center;
+    margin-bottom: 100px;
+    
+}
+h1{
+    font-size: 5rem;
+    font-family: tan meringue;
+}
+h2{
+    font-size: 3rem;
+    font-family: deuxieme rang;
+}
+h3{
+    font-size: 3rem;
+    font-family: tan meringue;
+}
+
 .video-wrapper {
     /* Telling our absolute positioned video to 
     be relative to this element */
@@ -54,6 +81,14 @@ video {
     flex-direction: column;
     align-items: center;
     justify-content: center;
+}
+
+.countdown {
+  display: flex;
+  font-size: 40px;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
 }
 </style>
 
