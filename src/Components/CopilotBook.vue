92648fb81cb1a0581429ee8cf4b4ed54168a622e
Carrybou <thomasguibert94@gmail.com>
2024-05-13 11:56:57 +0200

    parchemin mobil

--- src/Components/CopilotBook.vue
+++ src/Components/CopilotBook.vue
@@ -19,17 +19,17 @@ export default {
         return {
             pages: [
                 { title: 'Page 1', 
-                content: '<div class="left" style=" margin-left:20px"><div><h2 style="color: black;">Diva Faune</h2><p>Diva Faune est un groupe d’électro-pop français, fondé en 2013 par deux amis, dont un diplômé de l\'UTT.C’est avec le titre “Shine on my way” que le groupe se fait connaître du grand public et leur EP “Get up” est certifié disque de platine. </p></div><iframe style=" margin-left:20px; border-radius:12px;" src="https://open.spotify.com/embed/artist/39l75cUNbVMpHgykQLyOUT?utm_source=generator" width="90%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>' },
-                { title: 'Page 2', content: '<img src="/artiste/diva faune.png" style="margin:10px background: top no-repeat; background-size:cover;"/>' },
+                content: '<div class="left" style=" margin-left:20px"><div><h2 style="color: black;">Diva Faune</h2><p>Diva Faune est un groupe d’électro-pop français, fondé en 2013 par deux amis, dont un diplômé de l\'UTT.C’est avec le titre “Shine on my way” que le groupe se fait connaître du grand public et leur EP “Get up” est certifié disque de platine. </p></div><iframe class="spotify" style=" margin-left:20px; border-radius:12px;" src="https://open.spotify.com/embed/artist/39l75cUNbVMpHgykQLyOUT?utm_source=generator" width="90%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>' },
+                { title: 'Page 2', content: '<div class="right"><img src="/artiste/diva faune.png" style="margin:10px background: top no-repeat; background-size:cover;"/></div>' },
                 { title: 'Page 3', 
-                content: '<div style=" margin-left:20px"><h2 style="color: black;">Bekar</h2><p>Après avoir évolué au sein d’un groupe, il a entamé une carrière solo marquée par la sortie de son premier projet, « Boréal ». Il sort ensuite sa mixtape « Briques rouges », dans laquelle Bekar se présente musicalement.</p><iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6wjkiUBMMYSIx5UBTp7eKC?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>' },
-                { title: 'Page 4', content: '<img src="artiste/bekar.jpeg" style="background: top no-repeat; background-size:cover;"/>' },
+                content: '<div style=" margin-left:20px"><h2 style="color: black;">Bekar</h2><p>Après avoir évolué au sein d’un groupe, il a entamé une carrière solo marquée par la sortie de son premier projet, « Boréal ». Il sort ensuite sa mixtape « Briques rouges », dans laquelle Bekar se présente musicalement.</p><iframe class="spotify" style="border-radius:12px" src="https://open.spotify.com/embed/artist/6wjkiUBMMYSIx5UBTp7eKC?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>' },
+                { title: 'Page 4', content: '<div class="right"><img src="artiste/bekar.jpeg" style="background: top no-repeat; background-size:cover;"/></div>' },
                 { title: 'Page 5', 
-                content: '<div style=" margin-left:20px"><h2 style="color: black;">Chinwvr</h2><p>Un ciel dégagé, un coucher de soleil jaune orangé et un bon jus d\'ananas bien frais: Chinwvr n\'attendra pas l\'été pour vous amener dans son univers estivale.</p><iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/0FO41jGlT3izLX7oULFUlr?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>' },
-                { title: 'Page 6', content: '<img src="artiste/chinwvr.png" style="background: top no-repeat; background-size:cover;"/>' },
+                content: '<div style=" margin-left:20px"><h2 style="color: black;">Chinwvr</h2><p>Un ciel dégagé, un coucher de soleil jaune orangé et un bon jus d\'ananas bien frais: Chinwvr n\'attendra pas l\'été pour vous amener dans son univers estivale.</p><iframe class="spotify" style="border-radius:12px" src="https://open.spotify.com/embed/artist/0FO41jGlT3izLX7oULFUlr?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>' },
+                { title: 'Page 6', content: '<div class="right"><img src="artiste/chinwvr.png" style="background: top no-repeat; background-size:cover;"/></div>' },
                 { title: 'Page 7', 
-                content: '<div style=" margin-left:20px"><h2 style="color: black;">Arone</h2><p>Arøne fait partie de « la next gen », cette génération d’artistes ayant grandi en écoutant majoritairement du rap, de la pop ou encore de l’électro. Elle propose aujourd’hui une musique touchante et rythmée. </p><iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/4azuvIO6w5uQmthX95jfL8?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>' },
-                { title: 'Page 8', content: '<img src="artiste/arone.png" style="background: top no-repeat; background-size:cover ;"/>' },
+                content: '<div style=" margin-left:20px"><h2 style="color: black;">Arone</h2><p>Arøne fait partie de « la next gen », cette génération d’artistes ayant grandi en écoutant majoritairement du rap, de la pop ou encore de l’électro. Elle propose aujourd’hui une musique touchante et rythmée. </p><iframe class="spotify" style="border-radius:12px" src="https://open.spotify.com/embed/artist/4azuvIO6w5uQmthX95jfL8?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>' },
+                { title: 'Page 8', content: '<div class="right"><img src="artiste/arone.png" style="background: top no-repeat; background-size:cover ;"/></div>' },
                 // Add more pages as needed
             ],
             currentPage: 0,
@@ -78,15 +78,13 @@ export default {
 
 .page-container {
     display: flex;
-    justify-content: center;
     gap: 30px;
 
 }
 
 .page {
     display: flex;
-    justify-content: center;
-    align-items: center;
+    
     background-color: none;
 
     width:400px;
@@ -99,31 +97,47 @@ export default {
 
 }
 
-@media (min-width: 601px) and (max-width: 900px) {
-    
-    .page {
-        width: 80%;
-        height: 100%;
-    }
-}
 
-@media (max-width: 600px) {
+
+@media (max-width: 900px) {
     .book {
-        width: 100%;
-        height: auto;
-        .left{
-            margin-left: 0px;
-            display: flex;
-            flex-direction: row;
+        width: 360px;
+        height: 550px;
+        background-image: url("/artiste/parchemin.png") ;
+        padding-top: 10px;
+
+        h2{
+            margin-top: 15px;
+            margin-left: 35px;
         }
+        p{
+            margin: 10px;
+            padding: 15px;
+        }
+    }
+    .navigation {
+        bottom: 30px;
+        flex-direction: row;
+    }
+    .spotify{
+        height: 100px;
     }
     .page-container {
         flex-direction: column;
     }
     .page {
+        padding-top: 10px;
         width: 90%;
         height: auto;
 
+    }
+    .left{
+            margin-left: 0px;
+            display: flex;
+            flex-direction: column;
+        }
+    .right{
+        display: none;
     }
     .navigation {
         flex-direction: column;
